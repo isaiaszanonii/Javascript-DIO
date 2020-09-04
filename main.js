63eb623af63b69eb25472b7c6d87697c94d7c0b9
isaiaszanoni <isaiasz.mus@gmail.com>
2020-09-04 16:20:32 -0300

    aula 02: listas, dicionarios e algumas outras funcoes.

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-var nome = "Isaías";
-var n_curso = 1
-frase = "Palmeiras é o melhor time";
-alert(frase.toUpperCase());
+// Lista de dicionários:
+
+frutas = [{nome: "maça", cor:"vermelha"}, {nome:"uva", cor:"verde"}]
+console.log(frutas)
+console.log(frutas[1].cor)
+
+/* - Dicionário
+
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+
+
+ - Lista como string
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+
+- Separação por traços ou qualquer outra fita:
+console.log(lista.join("; "))
+ 
+
+
+ - push e pop
+
+console.log("Imprimindo a lista: " + lista);
+console.log("Imprimindo o item 2: " + lista[1]);
+
+lista.push("Banana");
+console.log("Adicionamos mais uma fruta, veja a lista:");
+console.log(lista);
+
+console.log("Uma fruta acabou: " + lista[3]);
+lista.pop()
+console.log("Agora temos: " + lista) */
