8df5cb1331a131cc7af627261484fe6614912174
isaiaszanoni <isaiasz.mus@gmail.com>
2020-09-04 21:30:19 -0300

    Aula 03: Estruturas de repeticao, input e condicional

--- main.js
+++ main.js
@@ -1,4 +1,81 @@
-var nome = "Isaías";
-var n_curso = 1
-frase = "Palmeiras é o melhor time";
-alert(frase.toUpperCase());
+var lista  = [nome: “rosa” , “cravo” , “tulipa”, “margarida”]
+console.log(lista[2]);
+
+/*
+
+var d = new Date();
+console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));
+
+/* - Date
+
+var d = new Date();
+alert(d.getMinutes()+2);
+console.log(d.getMonth()+1);
+console.log(d.getDay());
+console.log(d.getMinutes());
+/*
+var count;
+for(count=0; count <=5; count++){
+    console.log(count);
+};
+
+/* - While
+
+var count = 0;
+while (count <= 10){
+    console.log(count);
+    count++
+}
+
+
+
+ - Recebendo uma entrada - Prompt
+var idade = prompt("Qual a sua idade? ");
+if (idade >= 18){
+    console.log("Maior de idade.");
+}
+else{
+    console.log("menor de idade.")
+}
+
+
+
+ Lista de dicionários:
+
+frutas = [{nome: "maça", cor:"vermelha"}, {nome:"uva", cor:"verde"}];
+
+
+// console.log(frutas);
+// console.log(frutas[1].cor);
+
+ - Dicionário
+
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+
+
+ - Lista como string
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+
+- Separação por traços ou qualquer outra fita:
+console.log(lista.join("; "))
+ 
+
+
+ - push e pop
+
+console.log("Imprimindo a lista: " + lista);
+console.log("Imprimindo o item 2: " + lista[1]);
+
+lista.push("Banana");
+console.log("Adicionamos mais uma fruta, veja a lista:");
+console.log(lista);
+
+console.log("Uma fruta acabou: " + lista[3]);
+lista.pop()
+console.log("Agora temos: " + lista) */
